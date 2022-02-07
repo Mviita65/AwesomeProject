90e2ff7e10943a981bc7d4b85d5ae69799c0a597
Mviita65 <mviita65@gmail.com>
2022-02-07 09:51:28 +0200

    Timer and start new ok

--- App.js
+++ App.js
@@ -1,28 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './components/styles';
 
+
 const App = () => {
-  const [count, setCount] = useState(0);
-  const timer = 10;
+  const [time, setTime] = useState(0);
+  const [clickCounter, setClickCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (time > 0) {
+        setTime(time-1)
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [time]);
+
 
   return (
     <View style={styles.container}>
       <Text style={styles.caption2}>Welcome to the </Text>
       <Text style={styles.caption1}>Clicking Game!</Text>
       <Text style={styles.text}>Click "Start new" to begin!</Text>
-      <Text style={styles.text}>You have 10 seconds to click, time left: {timer} s</Text>
-      <Text style={styles.caption2}>You manage to click {count} times</Text>
+      <Text style={styles.text}>You have 10 seconds to click, time left: {time} s</Text>
+      <Text style={styles.caption2}>You manage to click {clickCounter} times</Text>
       <View style={styles.rowContainer}>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => setCount(count + 1)}
+          onPress={() => {
+            if (time > 0) {
+              setClickCounter(clickCounter + 1)
+            }
+          }}
           >
           <Text style={styles.buttonText}>Click me</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => setCount(0)}
+          onPress={() => {setClickCounter(0);setTime(10) }}
           >
           <Text style={styles.buttonText}>Start new</Text>
         </TouchableOpacity>
