ca484d1e607edf8253ad6ba6dd510b252b153081
Mviita65 <mviita65@gmail.com>
2022-02-17 10:29:35 +0200

    new highscore to highscorelist

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import { View, Text, TouchableOpacity, FlatList, ScrollView } from 'react-native';
 import styles from './components/styles';
+import checkHighScores from './components/checkhighscores';
 
 
 const Item = ({ item }) => (
@@ -14,9 +15,9 @@ const Item = ({ item }) => (
 
 const App = () => {
   const [time, setTime] = useState(0);
-  const [runtime, setRuntime] = useState(0);
   const [clickCounter, setClickCounter] = useState(0);
- 
+  const [trackHighScore, setTrackHighScore] = useState(false);
+
   const [highScores, setHighScores] = useState([
     { id: 1, name: 'Name', score: 10 },
     { id: 2, name: 'Name', score: 9 },
@@ -32,23 +33,22 @@ const App = () => {
 
   const renderItem = ({ item }) => (
     <Item item={item} />
-  );
-
+  ); 
+  
   useEffect(() => {
     const interval = setInterval(() => {
-      setRuntime(runtime+1)
       if (time > 0) {     // reduce the time if there is time left
         setTime(time-1)
       }
     }, 1000);
     return () => clearInterval(interval); // clears the scheduled interval once the component unmounts
-  }, [time,runtime,highScores]);
+  }, [time]);
 
   return (
     <View style={styles.container}>
       <Text style={styles.caption1}>Clicking Game!</Text>
       <Text style={styles.text}>Click "Start new" to begin!</Text>
-      <Text style={styles.text}>You have 10 seconds to click! </Text>
+      <Text style={styles.text}>You have 5 seconds to click! </Text>
       <Text style={styles.caption2}>Clicks: {clickCounter}, time left: {time} s</Text>
       <View style={styles.rowContainer}>
         <TouchableOpacity
@@ -57,9 +57,8 @@ const App = () => {
             if (time > 0) { // counts clicks if there is time left
               setClickCounter(clickCounter + 1)
             } else {
-
+              checkHighScores(highScores,setHighScores,clickCounter,trackHighScore,setTrackHighScore)
             }
-
           }}
           >
           <Text style={styles.buttonText}>Click me</Text>
@@ -68,7 +67,8 @@ const App = () => {
           style={styles.button}
           onPress={() => {  // resets the counter to zero and give 10 seconds playing time
             setClickCounter(0)
-            setTime(10) 
+            setTime(5) 
+            setTrackHighScore(true)
           }}
           >              
           <Text style={styles.buttonText}>Start new</Text>
@@ -76,15 +76,12 @@ const App = () => {
       </View>
       <View style={styles.highScores}>
         <Text style={styles.boldText}>High scores:</Text>
-        <ScrollView>
         <FlatList
           data={highScores}
           renderItem={renderItem}
           keyExtractor={item => item.id}
         />
-        </ScrollView>
       </View>
-      <Text style={styles.text}>Game has been up for {runtime} seconds!</Text>
       <Text style={styles.text}>© Mika Viitaniemi 2022</Text>
     </View>
   );
---
+++ components/checkhighscores.js
@@ -0,0 +1,26 @@
+//Checks if there are enough clicks to rewrite highScores and if so creates the new highScores list
+
+const checkHighScores = (
+    highScores,setHighScores,
+    clickCounter,
+    trackHighScore,setTrackHighScore
+) => {
+    let check = 0   // zero(0) indicates no highScore found and one(1) indicates new highScore found 
+    let tempScores = JSON.parse(JSON.stringify(highScores)) // to make a work copy
+    for (let i=0;i<10;i++){     // to check all highScores and get the one for rewriting
+        if (tempScores[i].score < clickCounter && trackHighScore && check===0){ 
+            check = 1                   // enough clicks to rewrite highscores found
+            setTrackHighScore(false)
+            let oldScore = tempScores[i].score  // old highScore value to pass downward
+            tempScores[i].score = clickCounter  // new value to this place
+            let start = i+1
+            for (let j=start;j<10;j++){         // to rewrite the rest of the highscorelist
+                let tempScore = tempScores[j].score
+                tempScores[j].score = oldScore
+                oldScore = tempScore
+            }
+            setHighScores(tempScores)           // work copy to replace highscorelist
+        }
+    }
+}
+export default checkHighScores;
